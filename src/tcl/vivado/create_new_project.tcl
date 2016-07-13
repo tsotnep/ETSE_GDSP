37dd1e0f4a8ed7524d4a7f7fd2352b5aa8c9dbfd
Tsotne Putkaradze <tsotnep@gmail.com>
2016-07-13 17:14:02 +0400

    now this file creates new project and creates *.prj file as well

---
+++ src/tcl/vivado/create_new_project.tcl
@@ -0,0 +1,43 @@
+#this script creates a new project, that later can be used for simulations
+
+source ../generics.tcl
+
+#close any project if it's open already
+    close_project -quiet
+    close_sim -force -quiet
+
+#create new project
+    create_project $PROJECTNAME $PROJECTLOCATION -part xc7z020clg484-1 -force
+    set_property board_part em.avnet.com:zed:part0:1.3 [current_project]
+    set_property target_language VHDL [current_project]
+
+
+#add hdl sources
+    add_files -norecurse -scan_for_includes $ORIGIN/src/hdl/CONTROL_UNIT_${PROJECTNAME}.vhd
+    add_files -norecurse -scan_for_includes $ORIGIN/src/hdl/BRAM_WRAPPER_V2.vhd
+    add_files -norecurse -scan_for_includes $ORIGIN/src/hdl/STANDARD_RAM.vhd
+    add_files -norecurse -scan_for_includes $ORIGIN/src/hdl/MATRIX_MUL_IP_CORE_${PROJECTNAME}.vhd
+    add_files -norecurse -scan_for_includes $ORIGIN/src/lib/txt_util.vhd
+    add_files -norecurse -scan_for_includes $ORIGIN/src/lib/MATRIX_MUL_IP_CORE_LIBRARY_Syn.vhd
+    add_files -norecurse -scan_for_includes $ORIGIN/src/lib/MATRIX_MUL_IP_CORE_LIBRARY_Sim.vhd
+    add_files -fileset sim_1 -norecurse -scan_for_includes /home/tsotne/git/ETSE_GDSP/src/tb/TB_MATRIX_MUL_IP_CORE_${PROJECTNAME}.vhd
+
+#add IP
+    add_files -norecurse $ORIGIN/src/ip/vivado/DSP_INPUT_C/DSP_INPUT_C.xci
+    add_files -norecurse $ORIGIN/src/ip/vivado/BRAM18x1k/BRAM18x1k.xci
+
+#update sources
+    update_compile_order -fileset sources_1
+
+#set MATRIX_MUL_IP_CORE_${PROJECTNAME} as top module
+    set_property top MATRIX_MUL_IP_CORE_${PROJECTNAME} [current_fileset]
+
+#set TB_MATRIX_MUL_IP_CORE_${PROJECTNAME} as top module for simulation
+    set_property top TB_MATRIX_MUL_IP_CORE_${PROJECTNAME} [get_filesets sim_1]
+
+#we launch simulation in order to create *.prj file, that will be used for simulations in loop
+    launch_simulation
+
+
+#close the project
+    close_project

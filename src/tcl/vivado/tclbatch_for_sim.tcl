85f265671935838518b961ddab5ff0976d452d34
Tsotne Putkaradze <tsotnep@gmail.com>
2016-07-18 18:31:11 +0400

    now it will for for every testbench, all conditions has been added

--- src/tcl/vivado/tclbatch_for_sim.tcl
+++ src/tcl/vivado/tclbatch_for_sim.tcl
@@ -2,19 +2,26 @@
 #this script is executed during simulation, it defines stop statement and exits simulation
 
 #tell simulator when to stop
-    add_condition -name StoppingCondition1 {/TB_MATRIX_MUL_IP_CORE_S_INT/when_to_stop_simulation_flag == 1 } {
-        puts "Condition when_to_stop_simulation_flag==1 was encountered at [current_time]. Stopping simulation."
-        close_sim}
 
+    if { [catch { add_condition -name StoppingCondition0 {/TB_MATRIX_MUL_IP_CORE_S_INT/when_to_stop_simulation_flag == 1 } {
+            puts "Condition when_to_stop_simulation_flag==1 was encountered at [current_time]. Stopping simulation."
+            close_sim} } err] } {puts ""} else { puts "\n\n**********STOPPING CONDITION ADDED**********\n\n" }
 
+    if { [catch { add_condition -name StoppingCondition1 {/TB_MATRIX_MUL_IP_CORE_S_INT_G/when_to_stop_simulation_flag == 1 } {
+            puts "Condition when_to_stop_simulation_flag==1 was encountered at [current_time]. Stopping simulation."
+            close_sim} } err] } {puts ""} else { puts "\n\n**********STOPPING CONDITION ADDED**********\n\n" }
+
+    if { [catch { add_condition -name StoppingCondition2 {/TB_MATRIX_MUL_IP_CORE_S_REAL/when_to_stop_simulation_flag == 1 } {
+            puts "Condition when_to_stop_simulation_flag==1 was encountered at [current_time]. Stopping simulation."
+            close_sim} } err] } {puts ""} else { puts "\n\n**********STOPPING CONDITION ADDED**********\n\n" }
+
+    if { [catch { add_condition -name StoppingCondition3 {/TB_MATRIX_MUL_IP_CORE_CV_INT/when_to_stop_simulation_flag == 1 } {
+            puts "Condition when_to_stop_simulation_flag==1 was encountered at [current_time]. Stopping simulation."
+            close_sim} } err] } {puts ""} else { puts "\n\n**********STOPPING CONDITION ADDED**********\n\n" }
+
+    if { [catch { add_condition -name StoppingCondition4 {/TB_MATRIX_MUL_IP_CORE_CV_REAL/when_to_stop_simulation_flag == 1 } {
+            puts "Condition when_to_stop_simulation_flag==1 was encountered at [current_time]. Stopping simulation."
+            close_sim} } err] } {puts ""} else { puts "\n\n**********STOPPING CONDITION ADDED**********\n\n" }
 
 #run forever until the condition above
     run -all
-
-
-#TODO: TB_MATRIX_MUL_IP_CORE_S_INT can NOT be set into proper varying value
-#1. sourcing generics and then : TB_MATRIX_MUL_IP_CORE_${PROJECTNAME}
-#2. by : set TB_TOP TB_MATRIX_MUL_IP_CORE_S_INT
-# this: puts [pwd] ->>>/home/tsotne/git/ETSE_GDSP/src/tcl/vivado
-#i don't know solution yet.
-#even if this problem will be fixed, for example, add multiple conditions simultaneously, and any of them will be correct, TB*.vhd files are not fixed yet.

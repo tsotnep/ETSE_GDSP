bc43549221af6bb06d1d5b72683fa82452d14ae2
Tsotne Putkaradze <tsotnep@gmail.com>
2016-08-08 14:53:04 +0200

    now it tests simple alone FIFO. works

--- src/app/helloworld.c
+++ src/app/helloworld.c
@@ -52,6 +52,7 @@
 #include "xbasic_types.h"
 #include "xil_io.h"
 #include "sleep.h"
+#include "testing_FIFO.h"
 
 void ResetCntrl(){
 
@@ -102,7 +103,7 @@ void LoadVals(int a0, int a1, int a2, int a3, int a4, int a5, int a6, int a7){
 	//continue to next state
 	MMULT_AXI_3_mWriteReg(XPAR_MMULT_AXI_3_0_S00_AXI_BASEADDR, MMULT_AXI_3_S00_AXI_SLV_REG31_OFFSET, 1);
 	MMULT_AXI_3_mWriteReg(XPAR_MMULT_AXI_3_0_S00_AXI_BASEADDR, MMULT_AXI_3_S00_AXI_SLV_REG31_OFFSET, 0);
-	xil_printf("P matrix loaded\n\r");
+	xil_printf("P matrix loaded\n\n\r");
 }
 
 void UnloadVals(){
@@ -121,36 +122,101 @@ void UnloadVals(){
 	xil_printf(" %d \n\r", r1);
 	xil_printf(" %d \n\r", r2);
 	xil_printf(" %d \n\r", r3);
-	xil_printf("Unload Content end:\n\r");
+	xil_printf("Unload Content end:\n\n\r");
 }
+
+void testMMULT_Controller(){
+
+	//test data 1
+		//reset MMULTs
+		ResetCntrl();
+		//load data
+		LoadVals(1,2,3,4, 1,2,3,4);
+		//wait a while, later i will add irq
+		sleep(2);
+		//unload
+		UnloadVals();
+
+	//wait
+		sleep(2);
+
+	//test data 2
+		//reset MMULTs
+		ResetCntrl();
+		//load data
+		LoadVals(1,2,3,4, 5,6,7,8);
+		//wait a while, later i will add irq
+		sleep(2);
+		//unload
+		UnloadVals();
+
+}
+
+
+
+
+/////////////////////////////////////////test FIFO
+void ReadTestFIFOVals(){
+	u32 r0 = TESTING_FIFO_mReadReg(XPAR_TESTING_FIFO_0_S00_AXI_BASEADDR, TESTING_FIFO_S00_AXI_SLV_REG0_OFFSET);
+	u32 r1 = TESTING_FIFO_mReadReg(XPAR_TESTING_FIFO_0_S00_AXI_BASEADDR, TESTING_FIFO_S00_AXI_SLV_REG1_OFFSET);
+	u32 r2 = TESTING_FIFO_mReadReg(XPAR_TESTING_FIFO_0_S00_AXI_BASEADDR, TESTING_FIFO_S00_AXI_SLV_REG2_OFFSET);
+	u32 r3 = TESTING_FIFO_mReadReg(XPAR_TESTING_FIFO_0_S00_AXI_BASEADDR, TESTING_FIFO_S00_AXI_SLV_REG3_OFFSET);
+	u32 r4 = TESTING_FIFO_mReadReg(XPAR_TESTING_FIFO_0_S00_AXI_BASEADDR, TESTING_FIFO_S00_AXI_SLV_REG4_OFFSET);
+	u32 r5 = TESTING_FIFO_mReadReg(XPAR_TESTING_FIFO_0_S00_AXI_BASEADDR, TESTING_FIFO_S00_AXI_SLV_REG5_OFFSET);
+	u32 r6 = TESTING_FIFO_mReadReg(XPAR_TESTING_FIFO_0_S00_AXI_BASEADDR, TESTING_FIFO_S00_AXI_SLV_REG6_OFFSET);
+	u32 r7 = TESTING_FIFO_mReadReg(XPAR_TESTING_FIFO_0_S00_AXI_BASEADDR, TESTING_FIFO_S00_AXI_SLV_REG7_OFFSET);
+	u32 r8 = TESTING_FIFO_mReadReg(XPAR_TESTING_FIFO_0_S00_AXI_BASEADDR, TESTING_FIFO_S00_AXI_SLV_REG8_OFFSET);
+
+	xil_printf("TestFIFO Reading start:\n\r");
+	xil_printf(" %d \n\r", r0);
+	xil_printf(" %d \n\r", r1);
+	xil_printf(" %d \n\r", r2);
+	xil_printf(" %d \n\r", r3);
+	xil_printf(" %d \n\r", r4);
+	xil_printf(" %d \n\r", r5);
+	xil_printf(" %d \n\r", r6);
+	xil_printf(" %d \n\r", r7);
+	xil_printf(" %d \n\r", r8);
+	xil_printf("TestFIFO Reading end:\n\n\r");
+}
+
+void WriteTestFIFOVals(u32 x){
+	TESTING_FIFO_mWriteReg(XPAR_TESTING_FIFO_0_S00_AXI_BASEADDR, TESTING_FIFO_S00_AXI_SLV_REG0_OFFSET, x);
+	xil_printf("TestFIFO value %d written:\n\n\r", x);
+}
+
+void test_testingFIFO(){
+	ReadTestFIFOVals();
+	WriteTestFIFOVals(1);
+
+
+	ReadTestFIFOVals();
+	WriteTestFIFOVals(2);
+
+
+	ReadTestFIFOVals();
+	WriteTestFIFOVals(3);
+
+	ReadTestFIFOVals();
+	WriteTestFIFOVals(4);
+
+
+	ReadTestFIFOVals();
+	WriteTestFIFOVals(5);
+
+
+	ReadTestFIFOVals();
+	WriteTestFIFOVals(6);
+}
+
+
+
 
 int main()
 {
+	test_testingFIFO();
 
-//test data 1
-	//reset MMULTs
-	ResetCntrl();
-	//load data
-	LoadVals(1,2,3,4,1,2,3,4);
-	//wait a while, later i will add irq
-	sleep(2);
-	//unload
-	UnloadVals();
-
-//wait
-	sleep(2);
-
-//test data 2
-	//reset MMULTs
-	ResetCntrl();
-	//load data
-	LoadVals(1,2,3,4,5,6,7,8);
-	//wait a while, later i will add irq
-	sleep(2);
-	//unload
-	UnloadVals();
 
 
     return 0;
 }
-

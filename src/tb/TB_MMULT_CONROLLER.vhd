c5351df52175224d1fa49470d9a36b86bd1b7394
Tsotne Putkaradze <tsotnep@gmail.com>
2016-08-08 02:21:28 +0200

    tb pretends axi

--- src/tb/TB_MMULT_CONROLLER.vhd
+++ src/tb/TB_MMULT_CONROLLER.vhd
@@ -26,6 +26,8 @@ architecture RTL of TB_MMULT_CONROLLER is
 
     signal cntlr_save_G_values_in    : STD_LOGIC; --slv_reg29
     signal cntlr_save_P_values_in    : STD_LOGIC; --slv_reg30
+    signal cntlr_send_result_in    : STD_LOGIC; --slv_reg30
+    
     signal clear_P_G_arrays_in : STD_LOGIC; --slv_reg30
 
     signal slv_reg0 : std_logic_vector(C_SLV_DWIDTH - 1 downto 0);
@@ -73,6 +75,7 @@ MMULT_CONTROLLER_inst : entity work.MMULT_CONTROLLER
         cntlr_RST_in           => cntlr_RST_in,
         cntlr_save_G_values_in => cntlr_save_G_values_in,
         cntlr_save_P_values_in => cntlr_save_P_values_in,
+        cntlr_send_result_in   => cntlr_send_result_in,
         LOAD_PG_in             => LOAD_PG_in,
         UN_LOAD_in             => UN_LOAD_in,
         P_in                   => P_in,
@@ -108,29 +111,118 @@ MMULT_CONTROLLER_inst : entity work.MMULT_CONTROLLER
         slv_reg3         <= (others => '0');
         cntlr_save_G_values_in <= '0';
         cntlr_save_P_values_in <= '0';
+        wait for period * 100;
+        
+        
         cntlr_RST_in <= '1';
         cntlr_RST_in <= '0' after period;
-        wait for period * 4;
+        wait for period ;
 
         slv_reg0 <= std_logic_vector(to_unsigned(1, C_SLV_DWIDTH));
         slv_reg1 <= std_logic_vector(to_unsigned(2, C_SLV_DWIDTH));
         slv_reg2 <= std_logic_vector(to_unsigned(3, C_SLV_DWIDTH));
         slv_reg3 <= std_logic_vector(to_unsigned(4, C_SLV_DWIDTH));
+        wait for period * 100;
         cntlr_save_G_values_in <= '1';
-        cntlr_save_G_values_in <= '0' after period;
-        wait for period;
+        wait for period * 100;
+        cntlr_save_G_values_in <= '0';
+        wait for period * 100;
+
+        slv_reg0 <= std_logic_vector(to_unsigned(1, C_SLV_DWIDTH));
+        slv_reg1 <= std_logic_vector(to_unsigned(2, C_SLV_DWIDTH));
+        slv_reg2 <= std_logic_vector(to_unsigned(3, C_SLV_DWIDTH));
+        slv_reg3 <= std_logic_vector(to_unsigned(4, C_SLV_DWIDTH));
+        wait for period * 100;
+        cntlr_save_P_values_in <= '1';
+        wait for period * 100; --this makes it return '4' an all register.
+        cntlr_save_P_values_in <= '0';
+        wait for period * 100;
+        
+        cntlr_send_result_in <= '1';
+        wait for period * 100;
+        
+        LOAD_PG_in <= "11";
+        wait for 1000 ns;
+        
+        
+        
+        
+        
+        cntlr_RST_in <= '1';
+        cntlr_RST_in <= '0' after period;
+        wait for period * 4;
 
         slv_reg0 <= std_logic_vector(to_unsigned(5, C_SLV_DWIDTH));
         slv_reg1 <= std_logic_vector(to_unsigned(6, C_SLV_DWIDTH));
         slv_reg2 <= std_logic_vector(to_unsigned(7, C_SLV_DWIDTH));
         slv_reg3 <= std_logic_vector(to_unsigned(8, C_SLV_DWIDTH));
+        cntlr_save_G_values_in <= '1';
+        cntlr_save_G_values_in <= '0' after period;
+        wait for period;
+
+        slv_reg0 <= std_logic_vector(to_unsigned(9, C_SLV_DWIDTH));
+        slv_reg1 <= std_logic_vector(to_unsigned(10, C_SLV_DWIDTH));
+        slv_reg2 <= std_logic_vector(to_unsigned(11, C_SLV_DWIDTH));
+        slv_reg3 <= std_logic_vector(to_unsigned(12, C_SLV_DWIDTH));
         cntlr_save_P_values_in <= '1';
         cntlr_save_P_values_in <= '0' after period;
         wait for period;
         
-
+        wait for period * 10;
+        LOAD_PG_in <= "11";
+        
+        
+        
+        
         wait;
 
     end process stimul;
 
 end architecture RTL;
+
+
+--MMULT_CONTROLLER_AXI_inst : entity work.MMULT_CONTROLLER
+--        generic map(
+--            C_SLV_DWIDTH => 32,
+--            COLUMN_TOTAL => 2
+--        )
+--        port map(
+--            CLK                    => CLK,
+--            cntlr_RST_in           => slv_reg17(0),
+--            cntlr_save_G_values_in => slv_reg29(0),
+--            cntlr_save_P_values_in => slv_reg30(0),
+--            cntlr_send_result_in   => slv_reg31(0),
+--            
+--            LOAD_PG_in             => slv_reg20(1 downto 0),
+--            UN_LOAD_in             => slv_reg21(0),
+--            P_in                   => slv_reg22(0),
+--            G_in                   => slv_reg23(0),
+--            Bank_sel_in            => slv_reg24(0),
+--            --in
+--            slv_reg0               => slv_reg0,
+--            slv_reg1               => slv_reg1,
+--            slv_reg2               => slv_reg2,
+--            slv_reg3               => slv_reg3,
+--            
+--            --out
+--            slv_reg4               => slv_reg4,
+--            slv_reg5               => slv_reg5,
+--            slv_reg6               => slv_reg6,
+--            slv_reg7               => slv_reg7,
+--            
+--            --not used
+--            slv_reg8               => slv_reg8,
+--            slv_reg9               => slv_reg9,
+--            slv_reg10              => slv_reg10,
+--            slv_reg11              => slv_reg11,
+--            slv_reg12              => slv_reg12,
+--            slv_reg13              => slv_reg13,
+--            slv_reg14              => slv_reg14,
+--            slv_reg15              => slv_reg15,
+--            slv_reg16              => slv_reg16,
+--            
+--            READY_out              => slv_reg25(0),
+--            OP_DONE_out            => slv_reg26(0),
+--            LOADING_DONE_out       => slv_reg27(0),
+--            UN_LOADING_DONE_out    => slv_reg28(0)
+--        );

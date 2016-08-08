0414b6dfd18fcb2e2cc74ca1b2db356a3555c127
Tsotne Putkaradze <tsotnep@gmail.com>
2016-08-08 02:22:48 +0200

    removed possibility to wait for user input on command, it should be specified before, since core continues working while waiting for user. clearly, temporary solution, added TODO

--- src/hdl/MMULT_CONTROLLER.vhd
+++ src/hdl/MMULT_CONTROLLER.vhd
@@ -12,6 +12,7 @@ entity MMULT_CONTROLLER is
          cntlr_RST_in           : in  STD_LOGIC; --connected to axi reset
          cntlr_save_G_values_in : in  STD_LOGIC; --slv_reg29
          cntlr_save_P_values_in : in  STD_LOGIC; --slv_reg30
+         cntlr_send_result_in   : in  STD_LOGIC; --slv_reg31
 
          LOAD_PG_in             : in  STD_LOGIC_VECTOR(1 downto 0); --slv_reg20
          UN_LOAD_in             : in  STD_LOGIC; --slv_reg21
@@ -52,7 +53,7 @@ architecture Behavioral of MMULT_CONTROLLER is
         cntrl_WAIT_RESET, cntrl_WAIT_P_delay, cntrl_WAIT_G_delay, cntrl_WAIT_UNLOAD,
         cntrl_SAVE_G_P,
         cntrl_LOAD_G, cntrl_LOAD_P,
-        cntrl_CALCULTE, cntrl_UNLOAD
+        cntrl_CALCULTE, cntrl_UNLOAD, cntrl_WRITE_RESULTS
     );
 
     --IP signals
@@ -69,93 +70,75 @@ architecture Behavioral of MMULT_CONTROLLER is
     signal OP_DONE         : std_logic;
     --IP signals
 
-    --helpers
-    signal READY_shiftreg_1 : std_logic;
-    signal READY_shiftreg_2 : std_logic;
-    --helpers
-
 
     --controller signals
     signal cntlr_input_arr_G              : t_BRAM_DATA_integer := (0, 0, 0, 0);
     signal cntlr_input_arr_P              : t_BRAM_DATA_integer := (0, 0, 0, 0);
     signal cntlr_output_arr_R             : t_BRAM_DATA_integer := (others => 0);
+
     signal cntrl_G_array_index            : integer             := 0;
     signal cntrl_P_array_index            : integer             := 0;
     signal cntrl_R_array_index            : integer             := 0;
+
     signal cntrl_P_saved                  : std_logic;
     signal cntrl_G_saved                  : std_logic;
     signal cntrl_G_loaded                 : std_logic;
     signal cntrl_P_loaded                 : std_logic;
     signal cntrl_G_loading                : std_logic;
     signal cntrl_P_loading                : std_logic;
+
     signal cntrl_state                    : mmult_cntrl_state;
-    constant cntrl_reset_length           : integer             := 2;
+
     signal cntrl_reset_length_count       : integer             := 0;
-    constant cntrl_P_loading_predelay     : integer             := 5;
-    constant cntrl_G_loading_predelay     : integer             := 0;
     signal cntrl_P_loading_predelay_count : integer             := 0;
     signal cntrl_G_loading_predelay_count : integer             := 0;
 
---controller signals
+    constant cntrl_P_loading_predelay     : integer             := 4;
+    constant cntrl_G_loading_predelay     : integer             := 0;
+    constant cntrl_reset_length           : integer             := 2;
 
+--controller signals
+    signal cntlr_save_G_values_in_old : std_logic;
+    signal cntlr_save_P_values_in_old : std_logic;
 
 begin
     cntrl_FSM : process(clk) is
     begin
         if rising_edge(clk) then
-            READY_shiftreg_1 <= READY;
-            READY_shiftreg_2 <= READY_shiftreg_1;
-
+            cntlr_save_G_values_in_old <= cntlr_save_G_values_in;
+            cntlr_save_P_values_in_old <= cntlr_save_P_values_in;
+            
             if cntlr_RST_in = '1' then
                 cntrl_state              <= cntrl_RESET_MMULT;
+
                 cntrl_reset_length_count <= 0;
-                cntrl_G_array_index      <= 0;
+                cntrl_P_loading_predelay_count <= 0;
+                cntrl_G_loading_predelay_count <= 0;
+
+                cntrl_G_array_index <= 0;
+                cntrl_P_array_index <= 0;
+                cntrl_R_array_index <= 0;
+
                 cntlr_input_arr_G        <= (others => 0);
                 cntlr_input_arr_P        <= (others => 0);
-                cntrl_P_saved            <= '0';
-                cntrl_G_saved            <= '0';
-                cntrl_P_loaded           <= '0';
-                cntrl_G_loaded           <= '0';
-                cntrl_P_loading          <= '0';
-                cntrl_G_loading          <= '0';
+                cntlr_output_arr_R        <= (others => 0);
+
+
+
+                cntrl_P_saved        <= '0';
+                cntrl_G_saved        <= '0';
+                cntrl_G_loaded       <= '0';
+                cntrl_P_loaded       <= '0';
+                cntrl_G_loading      <= '0';
+                cntrl_P_loading      <= '0';
+
+
+
+
+
             else
                 case cntrl_state is
-                    when cntrl_IDLE =>
-                        if LOAD_PG_in = "11" then
-                            cntrl_state <= cntrl_WAIT_UNLOAD;
-                            LOAD_PG     <= LOAD_PG_in;
-                            Bank_sel    <= '1';
-                            UN_LOAD     <= '1';
-                        end if;
-                    when cntrl_WAIT_G_delay =>
-                        DIN     <= (others => '0');
-                        LOAD_PG <= LOAD_G_CMD;
-                        if cntrl_G_loading_predelay_count <= cntrl_G_loading_predelay then
-                            cntrl_G_loading_predelay_count <= cntrl_G_loading_predelay_count + 1;
-                        else
-                            cntrl_state <= cntrl_LOAD_G;
-                        end if;
-                    when cntrl_WAIT_P_delay =>
-                        DIN     <= (others => '0');
-                        LOAD_PG <= LOAD_P_CMD;
-                        if cntrl_P_loading_predelay_count <= cntrl_P_loading_predelay then
-                            cntrl_P_loading_predelay_count <= cntrl_P_loading_predelay_count + 1;
-                        else
-                            cntrl_state <= cntrl_LOAD_P;
-                        end if;
-                    when cntrl_WAIT_UNLOAD =>
-                        if READY = '1' and op_done = '1' then
-                            cntrl_state <= cntrl_UNLOAD;
-                        end if;
-                    when cntrl_WAIT_RESET  =>
-                        --wait until the IP is resetted, 2 clock cycles
-                        rst     <= '0';
-                        LOAD_PG <= (others => '1');
-                        if cntrl_reset_length_count < cntrl_reset_length - 1 then
-                            cntrl_reset_length_count <= cntrl_reset_length_count + 1;
-                        else
-                            cntrl_state <= cntrl_WAIT_G_delay;
-                        end if;
+
                     when cntrl_RESET_MMULT =>
                         --reset MMULT
                         Bank_sel    <= '0';
@@ -168,16 +151,24 @@ begin
                         DIN         <= (others => '0');
                         --reset MMULT
                         cntrl_state <= cntrl_SAVE_G_P;
+
+
                     when cntrl_SAVE_G_P =>
-                        if cntlr_save_P_values_in = '1' then
+                        if cntrl_P_saved = '1' and cntrl_G_saved = '1' and cntlr_send_result_in = '1' then
+                            cntrl_state <= cntrl_WAIT_RESET;
+                        end if;
+--                        if cntlr_save_P_values_in = '1' then
+                            if cntlr_save_P_values_in_old = '0' and cntlr_save_P_values_in = '1' then
+--                         if rising_edge(cntlr_save_P_values_in) then
                             cntlr_input_arr_P(0) <= to_integer(unsigned(slv_reg0));
                             cntlr_input_arr_P(1) <= to_integer(unsigned(slv_reg1));
                             cntlr_input_arr_P(2) <= to_integer(unsigned(slv_reg2));
                             cntlr_input_arr_P(3) <= to_integer(unsigned(slv_reg3));
                             cntrl_P_saved        <= '1';
                         end if;
-
-                        if cntlr_save_G_values_in = '1' then
+--                        if cntlr_save_G_values_in = '1' then
+                            if cntlr_save_G_values_in_old = '0' and cntlr_save_G_values_in = '1' then
+--                         if rising_edge(cntlr_save_G_values_in) then
                             cntlr_input_arr_G(0) <= to_integer(unsigned(slv_reg0));
                             cntlr_input_arr_G(1) <= to_integer(unsigned(slv_reg1));
                             cntlr_input_arr_G(2) <= to_integer(unsigned(slv_reg2));
@@ -185,9 +176,31 @@ begin
                             cntrl_G_saved        <= '1';
                         end if;
 
-                        if cntrl_P_saved = '1' and cntrl_G_saved = '1' then
-                            cntrl_state <= cntrl_WAIT_RESET;
+                    when cntrl_WAIT_RESET =>
+                        --wait until the IP is resetted, 2 clock cycles
+                        rst     <= '0';
+                        LOAD_PG <= (others => '1');
+                        cntrl_P_saved <= '0';
+                        cntrl_G_saved <= '0';
+                        if cntrl_reset_length_count < cntrl_reset_length - 1 then
+                            cntrl_reset_length_count <= cntrl_reset_length_count + 1;
+                        else
+                            cntrl_state <= cntrl_WAIT_G_delay;
                         end if;
+
+
+                    when cntrl_WAIT_G_delay =>
+                        DIN     <= (others => '0');
+                        LOAD_PG <= LOAD_G_CMD;
+                        if cntrl_G_loading_predelay_count <= cntrl_G_loading_predelay then
+                            cntrl_G_loading_predelay_count <= cntrl_G_loading_predelay_count + 1;
+                        else
+                            cntrl_state <= cntrl_LOAD_G;
+                        end if;
+
+
+
+
                     when cntrl_LOAD_G =>
                         DIN     <= (others => '0');
                         LOAD_PG <= LOAD_G_CMD;
@@ -200,6 +213,19 @@ begin
                                 cntrl_state <= cntrl_WAIT_P_delay;
                             end if;
                         end if;
+
+
+                    when cntrl_WAIT_P_delay =>
+                        DIN     <= (others => '0');
+                        LOAD_PG <= LOAD_P_CMD;
+                        if cntrl_P_loading_predelay_count <= cntrl_P_loading_predelay then
+                            cntrl_P_loading_predelay_count <= cntrl_P_loading_predelay_count + 1;
+                        else
+                            cntrl_state <= cntrl_LOAD_P;
+                        end if;
+
+
+
                     when cntrl_LOAD_P =>
                         if cntrl_P_array_index <= COLUMN_TOTAL * COLUMN_TOTAL - 1 then
                             DIN                 <= std_logic_vector(to_unsigned(cntlr_input_arr_P(cntrl_P_array_index), DATA_WIDTH));
@@ -209,18 +235,54 @@ begin
                                 cntrl_state <= cntrl_IDLE;
                             end if;
                         end if;
+
+
+                    when cntrl_IDLE =>
+--                        rst <= '0';
+--                        if LOAD_PG_in = "11" then  
+                            --TODO: here, LOAD_PG_in should be set(from user perspective) before it comes here, because it should not wait
+                            cntrl_state <= cntrl_WAIT_UNLOAD;
+                            LOAD_PG     <= "11";
+                            Bank_sel    <= '1';
+                            UN_LOAD     <= '1';
+--                        end if;
+
+
+                    when cntrl_WAIT_UNLOAD =>
+                        if loading_done = '1' then
+                            cntrl_state <= cntrl_UNLOAD;
+                        end if;
+
+
+
+                    when cntrl_UNLOAD =>
+                        if READY = '1' then
+                            if cntrl_R_array_index <= COLUMN_TOTAL * COLUMN_TOTAL - 1 then
+                                cntlr_output_arr_R(cntrl_R_array_index) <= to_integer(unsigned(DOUT));
+                                cntrl_R_array_index                     <= cntrl_R_array_index + 1;
+                            else
+                                cntrl_state <= cntrl_WRITE_RESULTS;
+                            end if;
+                        end if;
+
+
+                    when cntrl_WRITE_RESULTS =>
+                        slv_reg4 <= std_logic_vector(to_unsigned(cntlr_output_arr_R(0), C_SLV_DWIDTH));
+                        slv_reg5 <= std_logic_vector(to_unsigned(cntlr_output_arr_R(1), C_SLV_DWIDTH));
+                        slv_reg6 <= std_logic_vector(to_unsigned(cntlr_output_arr_R(2), C_SLV_DWIDTH));
+                        slv_reg7 <= std_logic_vector(to_unsigned(cntlr_output_arr_R(3), C_SLV_DWIDTH));
+                        cntrl_state <= cntrl_RESET_MMULT;
+
                     when cntrl_CALCULTE =>
                         null;
-                    when cntrl_UNLOAD =>
-                        if cntrl_R_array_index <= COLUMN_TOTAL * COLUMN_TOTAL - 1 then
-                            cntlr_output_arr_R(cntrl_R_array_index) <= to_integer(unsigned(DOUT));
-                            cntrl_R_array_index                     <= cntrl_R_array_index + 1;
-                        end if;
+
                 end case;
 
             end if;
         end if;
     end process cntrl_FSM;
+
+
 
     MATRIX_MUL_IP_CORE_S_INT_G_inst : entity work.MATRIX_MUL_IP_CORE_S_INT_G
         generic map(

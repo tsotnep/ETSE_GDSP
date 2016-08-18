6deb7ee80b6b02f9d8530b61df4bf5d6ac6052e1
Tsotne Putkaradze <tsotnep@gmail.com>
2016-08-18 11:51:00 +0200

    testbench update

--- src/tb/TB_MMULT_CONROLLER_2.vhd
+++ src/tb/TB_MMULT_CONROLLER_2.vhd
@@ -23,7 +23,7 @@ architecture RTL of TB_MMULT_CONROLLER_2 is
     constant period : time := 10 ns;
     signal clk      : std_logic;
 
-    signal WREN, RDEN                 : std_logic;
+    signal WREN, RDEN, RDY_FOR_CMD    : std_logic;
     signal WDATA, RDATA, RMATRIX_ADDR : std_logic_vector(C_S_AXI_DATA_WIDTH - 1 downto 0);
     signal RDADDR                     : std_logic_vector(1 downto 0);
 
@@ -32,14 +32,23 @@ architecture RTL of TB_MMULT_CONROLLER_2 is
     alias cmdin2 is WDATA(DATA_WIDTH + CMD_SIZE * 2 - 1 downto DATA_WIDTH + CMD_SIZE); --4 bits next command.
     alias cmdin3 is WDATA(C_S_AXI_DATA_WIDTH - 1 downto DATA_WIDTH + CMD_SIZE * 2); --6 bits spared for future.
 
-    constant cmd_RESET_CNTRL       : std_logic_vector := "1111";
-    constant cmd_SAVE_G            : std_logic_vector := "0001";
-    constant cmd_SAVE_P            : std_logic_vector := "0010";
-    constant cmd_LOAD_IN_MMULT     : std_logic_vector := "0011";
-    constant cmd_take_next_command : std_logic_vector := "0100";
-    constant cmd_UNLOAD            : std_logic_vector := "0101";
-    constant cmd_CALCULTE_PG       : std_logic_vector := "0110";
-    constant cmd_NULL              : std_logic_vector := "0111";
+    --cmd-s that affect state transition, they are read inside state: cmd_NULL
+    constant cmd_NULL              : std_logic_vector := std_logic_vector(to_unsigned(0, 4));
+    constant cmd_SAVE_G_or_P       : std_logic_vector := std_logic_vector(to_unsigned(1, 4));
+    constant cmd_LOAD_G            : std_logic_vector := std_logic_vector(to_unsigned(2, 4));
+    constant cmd_LOAD_P            : std_logic_vector := std_logic_vector(to_unsigned(3, 4));
+    constant cmd_CALCULTE          : std_logic_vector := std_logic_vector(to_unsigned(4, 4));
+    constant cmd_UNLOAD            : std_logic_vector := std_logic_vector(to_unsigned(5, 4));
+    constant cmd_PRINT             : std_logic_vector := std_logic_vector(to_unsigned(6, 4));
+    --7,8,9,10 
+    constant cmd_RESET_MMULT_IP    : std_logic_vector := std_logic_vector(to_unsigned(11, 4));
+    constant cmd_RESET_MMULT_CNTRL : std_logic_vector := std_logic_vector(to_unsigned(12, 4));
+
+    --cmd details inside states, they are read in state: cmd_SAVE_G_or_P
+    constant cmd_SAVE_G            : std_logic_vector := std_logic_vector(to_unsigned(13, 4));
+    constant cmd_SAVE_P            : std_logic_vector := std_logic_vector(to_unsigned(14, 4));
+    constant cmd_FINISH_SAVING_G_P : std_logic_vector := std_logic_vector(to_unsigned(15, 4));
+
     constant DOUT_SLV_REG1_ADRR    : std_logic_vector := std_logic_vector(to_unsigned(1, OPT_MEM_ADDR_BITS + 1));
     constant COLADDR_SLV_REG2_ADRR : std_logic_vector := std_logic_vector(to_unsigned(2, OPT_MEM_ADDR_BITS + 1));
 
@@ -52,10 +61,10 @@ architecture RTL of TB_MMULT_CONROLLER_2 is
     constant cmd_CALCULATE_PGt_LOWER  : std_logic_vector := "0001";
     constant cmd_CALCULATE_PGt_HIGHER : std_logic_vector := "0101";
 
-                                --cmd_next_command(3) 1=read or 0=write
-                                --cmd_next_command(2) 1=upperbank or 0=lowerbank
-                                --cmd_next_command(1 downto 0) -> what kind of calculation 00=PG
-                                
+    --cmd_next_command(3) 1=read or 0=write
+    --cmd_next_command(2) 1=upperbank or 0=lowerbank
+    --cmd_next_command(1 downto 0) -> what kind of calculation 00=PG
+
 
     procedure simulate_AXI_write(
         constant val      : in  integer;
@@ -121,6 +130,7 @@ begin
             RDEN         => RDEN,       --slv_reg_rden
 
             --out rd
+            RDY_FOR_CMD  => RDY_FOR_CMD,
             RDATA        => RDATA,      --slv_reg1
             RMATRIX_ADDR => RMATRIX_ADDR --slv_reg2
         );
@@ -129,13 +139,19 @@ begin
     begin
         WREN   <= '0';
         datain <= (others => '0');
-        cmdin  <= cmd_RESET_CNTRL;
+        cmdin  <= cmd_NULL;
         cmdin2 <= (others => '0');
         cmdin3 <= (others => '0');
         RDADDR <= (others => '0');
         RDEN   <= '0';
-        wait for period * 10;
 
+        wait for period * 20;
+
+        simulate_AXI_write(0, cmd_RESET_MMULT_CNTRL, cmd_NULL, WREN, cmdin, cmdin2, datain);
+        wait for period * 30;           --wait until RDY_FOR_CMD = '1';
+
+        simulate_AXI_write(0, cmd_SAVE_G_or_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
+        
         simulate_AXI_write(11, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
         simulate_AXI_write(12, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
         simulate_AXI_write(13, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
@@ -173,16 +189,16 @@ begin
 --        simulate_AXI_write(7, cmd_SAVE_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
 --        simulate_AXI_write(8, cmd_SAVE_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
 --        simulate_AXI_write(9, cmd_SAVE_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
-
+--
 --        simulate_AXI_write(1, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
---        simulate_AXI_write(2, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
---        simulate_AXI_write(3, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
---        simulate_AXI_write(4, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
---        simulate_AXI_write(5, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
---        simulate_AXI_write(6, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
---        simulate_AXI_write(7, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
---        simulate_AXI_write(8, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
---        simulate_AXI_write(9, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
+--        simulate_AXI_write(1, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
+--        simulate_AXI_write(1, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
+--        simulate_AXI_write(1, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
+--        simulate_AXI_write(1, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
+--        simulate_AXI_write(1, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
+--        simulate_AXI_write(1, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
+--        simulate_AXI_write(1, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
+--        simulate_AXI_write(1, cmd_SAVE_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
 --        simulate_AXI_write(1, cmd_SAVE_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
 --        simulate_AXI_write(1, cmd_SAVE_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
 --        simulate_AXI_write(1, cmd_SAVE_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
@@ -192,23 +208,36 @@ begin
 --        simulate_AXI_write(1, cmd_SAVE_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
 --        simulate_AXI_write(1, cmd_SAVE_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
 --        simulate_AXI_write(1, cmd_SAVE_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
-        
-        simulate_AXI_write(0, cmd_LOAD_IN_MMULT, cmd_CALCULATE_PG_HIGHER, WREN, cmdin, cmdin2, datain);
---        simulate_AXI_write(0, cmd_LOAD_IN_MMULT, cmd_CALCULATE_PGt_HIGHER, WREN, cmdin, cmdin2, datain);
+
+        simulate_AXI_write(1, cmd_FINISH_SAVING_G_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
+        wait for period * 30;           --wait until RDY_FOR_CMD = '1';
+
+        simulate_AXI_write(0, cmd_LOAD_G, cmd_NULL, WREN, cmdin, cmdin2, datain);
+        wait for period * 30;           --wait until RDY_FOR_CMD = '1';
+
+        simulate_AXI_write(0, cmd_LOAD_P, cmd_NULL, WREN, cmdin, cmdin2, datain);
+        wait for period * 30;           --wait until RDY_FOR_CMD = '1';
 
 
-        wait for 2000 ns;
-        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
-        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
-        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
-        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
-        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
-        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
-        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
-        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
-        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
+        simulate_AXI_write(0, cmd_CALCULTE, cmd_CALCULATE_PG_HIGHER, WREN, cmdin, cmdin2, datain);
+        wait for period * 30;           --wait until RDY_FOR_CMD = '1';
 
-        --current output is correct according to Taras's printed results
+
+        simulate_AXI_write(0, cmd_UNLOAD, cmd_UNLOAD_HIGHER, WREN, cmdin, cmdin2, datain);
+        wait for period * 30;           --wait until RDY_FOR_CMD = '1';
+
+
+        simulate_AXI_write(0, cmd_PRINT, cmd_NULL, WREN, cmdin, cmdin2, datain);
+        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
+        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
+        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
+        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
+        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
+        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
+        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
+        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
+        simulate_AXI_read(DOUT_SLV_REG1_ADRR, RDEN, RDADDR);
+        wait for period * 30;           --wait until RDY_FOR_CMD = '1';
 
 
         wait;

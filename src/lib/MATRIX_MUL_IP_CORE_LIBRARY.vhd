837ce2122f9e7fa40f653b5da92794a5713815d0
Tsotne Putkaradze <tsotnep@gmail.com>
2016-09-15 11:26:12 +0200

    constants added for internal fsm

--- src/lib/MATRIX_MUL_IP_CORE_LIBRARY.vhd
+++ src/lib/MATRIX_MUL_IP_CORE_LIBRARY.vhd
@@ -30,9 +30,11 @@ CONSTANT DATA_WIDTH		: integer:=18;
     type t_BRAM_DATA is array (0 to COLUMN_TOTAL - 1) of std_logic_vector(DATA_WIDTH - 1 downto 0);
     signal BRAM_DATA : t_BRAM_DATA;
 
-    --------------PACKAGEDECLARATION
-    constant LOAD_P_CMD      : std_logic_vector(1 downto 0) := "01";
-    constant LOAD_G_CMD      : std_logic_vector(1 downto 0) := "00";
+    --------------PACKAGEDECLARATION OPERATE_CMD
+    constant LOAD_P_CMD  : std_logic_vector(1 downto 0) := "01";
+    constant LOAD_G_CMD  : std_logic_vector(1 downto 0) := "00";
+    constant IDLE_CMD    : std_logic_vector(1 downto 0) := "10";
+    constant OPERATE_CMD : std_logic_vector(1 downto 0) := "11";
 
     signal g_cnt_delay_ready: integer:=0;
 --signal g_DSP_P_OUT: std_logic_vector(47 downto 0);

cded36372763d1c2e66426bad2249196dc68b842
tsotnep <tsotnep@gmail.com>
2017-01-26 01:00:13 +0200

    removed garbage

--- src/tcl/vivado/scripts/ETSE_GDSP.tcl
+++ src/tcl/vivado/scripts/ETSE_GDSP.tcl
@@ -1,35 +1,35 @@
 #crteate project
-create_project ETSE_GDSP /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP -part xc7z020clg484-1 -force
+create_project ETSE_GDSP /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/projects/ETSE_GDSP -part xc7z020clg484-1 -force
 set_property board_part em.avnet.com:zed:part0:1.3 [current_project]
 set_property target_language VHDL [current_project]
 
 
 #add vhd files for synthesis
-add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/txt_util.vhd
-add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/MATRIX_MUL_IP_CORE_LIBRARY.vhd
-add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/STANDARD_RAM.vhd
-add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/BRAM_WRAPPER_V2.vhd
-add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/GRAM_address_generator.vhd
-add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/CONTROL_UNIT_S_INT_G.vhd
-add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/MATRIX_MUL_IP_CORE_S_INT_G.vhd
-add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/MMULT_CONTROLLER_2.vhd
-add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/ETSE_GDSP_AXI.vhd
-add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/ETSE_GDSP.vhd
+add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/txt_util.vhd
+add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/MATRIX_MUL_IP_CORE_LIBRARY.vhd
+add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/STANDARD_RAM.vhd
+add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/BRAM_WRAPPER_V2.vhd
+add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/GRAM_address_generator.vhd
+add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/CONTROL_UNIT_S_INT_G.vhd
+add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/MATRIX_MUL_IP_CORE_S_INT_G.vhd
+add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/MMULT_CONTROLLER_2.vhd
+add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/ETSE_GDSP_AXI.vhd
+add_files -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/ETSE_GDSP.vhd
 
 
 #add vhd files for simulation
-add_files -fileset sim_1 -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/tb/tb_axi_stream_simulation_master.vhd
-add_files -fileset sim_1 -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/tb/tb_axi_stream_simulation_slave.vhd
-add_files -fileset sim_1 -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/tb/TB_MMULT_CONTROLLER_2.vhd
-add_files -fileset sim_1 -norecurse -scan_for_includes /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/tb/waveform.wcfg
+add_files -fileset sim_1 -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/tb/tb_axi_stream_simulation_master.vhd
+add_files -fileset sim_1 -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/tb/tb_axi_stream_simulation_slave.vhd
+add_files -fileset sim_1 -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/tb/TB_MMULT_CONTROLLER_2.vhd
+add_files -fileset sim_1 -norecurse -scan_for_includes /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/tb/waveform.wcfg
 set_property top TB_MMULT_CONROLLER_2 [get_filesets sim_1]
 set_property top_lib xil_defaultlib [get_filesets sim_1]
 update_compile_order -fileset sim_1
 #disable those files, won't be synthesized. not even needs to be disabbled.
-#set_property is_enabled false [get_files  /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/tb/waveform.wcfg]
-#set_property is_enabled false [get_files  /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/tb/TB_MMULT_CONTROLLER_2.vhd]
-#set_property is_enabled false [get_files  /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/tb/tb_axi_stream_simulation_master.vhd]
-#set_property is_enabled false [get_files  /home/tsotne/ownCloud/git/axis_ETSE/ETSE_GDSP/src/hdl/tb/tb_axi_stream_simulation_slave.vhd]
+#set_property is_enabled false [get_files  /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/tb/waveform.wcfg]
+#set_property is_enabled false [get_files  /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/tb/TB_MMULT_CONTROLLER_2.vhd]
+#set_property is_enabled false [get_files  /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/tb/tb_axi_stream_simulation_master.vhd]
+#set_property is_enabled false [get_files  /home/tsotne/ownCloud/git/ETSE_GDSP/src/hdl/tb/tb_axi_stream_simulation_slave.vhd]
 #set_property top ETSE_GDSP [get_filesets sim_1]
 #set_property top_lib xil_defaultlib [get_filesets sim_1]
 
@@ -38,16 +38,16 @@ update_compile_order -fileset sim_1
 #add dsp and blockram xilinx ips
 create_ip -name xbip_dsp48_macro -vendor xilinx.com -library ip -version 3.0 -module_name DSP_INPUT_C
     set_property -dict [list CONFIG.instruction1 {A} CONFIG.instruction2 {A*B} CONFIG.instruction3 {A*B+P} CONFIG.instruction4 {A*B+C} CONFIG.instruction5 {A*B+P>>17} CONFIG.instruction6 {B*A} CONFIG.instruction7 {C} CONFIG.pipeline_options {Expert} CONFIG.creg_1 {false} CONFIG.creg_2 {false} CONFIG.creg_3 {false} CONFIG.creg_4 {false} CONFIG.creg_5 {false} CONFIG.areg_1 {true} CONFIG.areg_2 {true} CONFIG.areg_3 {true} CONFIG.areg_4 {true} CONFIG.breg_1 {true} CONFIG.breg_2 {true} CONFIG.breg_3 {true} CONFIG.breg_4 {true} CONFIG.opreg_1 {true} CONFIG.opreg_2 {true} CONFIG.opreg_3 {true} CONFIG.opreg_4 {true} CONFIG.opreg_5 {true} CONFIG.mreg_5 {true} CONFIG.preg_6 {true} CONFIG.d_width {18} CONFIG.a_width {18} CONFIG.a_binarywidth {0} CONFIG.b_width {18} CONFIG.b_binarywidth {0} CONFIG.concat_width {48} CONFIG.concat_binarywidth {0} CONFIG.c_width {48} CONFIG.c_binarywidth {0} CONFIG.pcin_binarywidth {0} CONFIG.p_full_width {48} CONFIG.p_width {48} CONFIG.p_binarywidth {0}] [get_ips DSP_INPUT_C]
-    generate_target {instantiation_template} [get_files /home/tsotne/ownCloud/git/ETSE_GDSP/project/vivado/S_INT_G/S_INT_G.srcs/sources_1/ip/DSP_INPUT_C/DSP_INPUT_C.xci]
+    #generate_target {instantiation_template} [get_files /home/tsotne/ownCloud/git/ETSE_GDSP/projects/vivado/S_INT_G/S_INT_G.srcs/sources_1/ip/DSP_INPUT_C/DSP_INPUT_C.xci]
 create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.3 -module_name BRAM18x1k
     set_property -dict [list CONFIG.Memory_Type {Simple_Dual_Port_RAM} CONFIG.Write_Width_A {18} CONFIG.Write_Depth_A {1024} CONFIG.Enable_A {Always_Enabled} CONFIG.Enable_B {Always_Enabled} CONFIG.Use_RSTB_Pin {true} CONFIG.Reset_Memory_Latch_B {true} CONFIG.Output_Reset_Value_B {1} CONFIG.Read_Width_A {18} CONFIG.Operating_Mode_A {NO_CHANGE} CONFIG.Write_Width_B {18} CONFIG.Read_Width_B {18} CONFIG.Register_PortA_Output_of_Memory_Primitives {false} CONFIG.Register_PortB_Output_of_Memory_Primitives {true} CONFIG.Port_B_Clock {100} CONFIG.Port_B_Enable_Rate {100}] [get_ips BRAM18x1k]
-    generate_target {instantiation_template} [get_files /home/tsotne/ownCloud/git/ETSE_GDSP/project/vivado/S_INT_G/S_INT_G.srcs/sources_1/ip/BRAM18x1k/BRAM18x1k.xci]
+    #generate_target {instantiation_template} [get_files /home/tsotne/ownCloud/git/ETSE_GDSP/projects/vivado/S_INT_G/S_INT_G.srcs/sources_1/ip/BRAM18x1k/BRAM18x1k.xci]
 
 
 #package this project
-ipx::package_project -root_dir /home/tsotne/ownCloud/git/axis_ETSE/ip_repo -vendor user.org -library user -taxonomy /UserIP -import_files -set_current false
-ipx::unload_core /home/tsotne/ownCloud/git/axis_ETSE/ip_repo/component.xml
-ipx::edit_ip_in_project -upgrade true -name tmp_edit_project -directory /home/tsotne/ownCloud/git/axis_ETSE/ip_repo /home/tsotne/ownCloud/git/axis_ETSE/ip_repo/component.xml
+  ipx::package_project -root_dir /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/ip_repo -vendor user.org -library user -taxonomy /UserIP -import_files -set_current false
+ipx::unload_core /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/ip_repo/component.xml
+ipx::edit_ip_in_project -upgrade true -name tmp_edit_project -directory /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/ip_repo /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/ip_repo/component.xml
 update_compile_order -fileset sources_1
 current_project ETSE_GDSP
 current_project tmp_edit_project
@@ -57,5 +57,5 @@ ipx::create_xgui_files [ipx::current_core]
 ipx::update_checksums [ipx::current_core]
 ipx::save_core [ipx::current_core]
 close_project -delete
-set_property  ip_repo_paths  {/home/tsotne/ownCloud/git/axis_ETSE/ip_repo /home/tsotne/ownCloud/git/axis_ETSE/ip_repo} [current_project]
+set_property  ip_repo_paths  {/home/tsotne/ownCloud/git/ETSE_GDSP/tmp/ip_repo /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/ip_repo} [current_project]
 update_ip_catalog

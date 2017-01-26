2d52ef6220422de3a060987a9df2b448a9082709
tsotnep <tsotnep@gmail.com>
2017-01-26 14:14:06 +0200

    updated scripts

--- src/tcl/vivado/ETSE_GDSP_DMA.tcl
+++ src/tcl/vivado/ETSE_GDSP_DMA.tcl
@@ -1,11 +1,22 @@
-create_project ETSE_GDSP_DMA /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/projects/ETSE_GDSP_DMA -part xc7z020clg484-1
+close_project -quiet
+set origin /home/tsotnep/ownCloud/git/ETSE_GDSP
+
+create_project ETSE_GDSP_DMA ${origin}/tmp/projects/ETSE_GDSP_DMA -part xc7z020clg484-1 -force
 set_property board_part em.avnet.com:zed:part0:1.3 [current_project]
 set_property target_language VHDL [current_project]
-set_property  ip_repo_paths  {/home/tsotne/ownCloud/git/ETSE_GDSP/tmp/ip_repo} [current_project]
-update_ip_catalog
-
 
 create_bd_design "design_1"
+
+#TODO: BUG, variable does not work in setting ip repo paths, so i set it manually
+#INFO: code works if typed manually. removing this bs directory didn't help in script, maybe it needs time to understand
+#set_property  ip_repo_paths  {} [current_project]
+#update_ip_catalog
+#set_property  ip_repo_paths  {${origin}/tmp/ip_repo} [current_project]
+#update_ip_catalog
+#WARNING: [IP_Flow 19-2248] Failed to load user IP repository '/home/tsotnep/.local/share/Trash/files/tmp/$origin/tmp/ip_repo'; Can't find the specified path. If this directory should no longer be in your list of user repositories, go to the IP Settings dialog and remove it.
+
+set_property  ip_repo_paths  {/home/tsotnep/ownCloud/git/ETSE_GDSP/tmp/ip_repo} [current_project]
+update_ip_catalog
 
 startgroup
 create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0
@@ -84,8 +95,8 @@ validate_bd_design
 regenerate_bd_layout
 save_bd_design
 
-make_wrapper -files [get_files /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.srcs/sources_1/bd/design_1/design_1.bd] -top
-add_files -norecurse /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+make_wrapper -files [get_files ${origin}/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.srcs/sources_1/bd/design_1/design_1.bd] -top
+add_files -norecurse ${origin}/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
 update_compile_order -fileset sources_1
 update_compile_order -fileset sim_1
 
@@ -95,9 +106,7 @@ launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 
 
-file mkdir /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.sdk
-file copy -force /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.runs/impl_1/design_1_wrapper.sysdef /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.sdk/design_1_wrapper.hdf
+file mkdir ${origin}/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.sdk
+file copy -force ${origin}/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.runs/impl_1/design_1_wrapper.sysdef ${origin}/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.sdk/design_1_wrapper.hdf
 
-launch_sdk -workspace /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.sdk -hwspec /home/tsotne/ownCloud/git/ETSE_GDSP/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.sdk/design_1_wrapper.hdf
-
-#-data /home/tsotne/workspaceVivado/Project_Name/Project_Name.sdk -vmargs -Dcom.xilinx.sdk.args.hwspec=/home/tsotne/workspaceVivado/Project_Name/Project_Name.sdk/Block_Design_wrapper.hdf -Xms64m -Xmx512m
+launch_sdk -workspace ${origin}/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.sdk -hwspec ${origin}/tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.sdk/design_1_wrapper.hdf

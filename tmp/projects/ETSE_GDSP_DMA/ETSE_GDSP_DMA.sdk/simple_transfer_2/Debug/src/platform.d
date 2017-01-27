350127301bd2db6e6a473f41eea9c448e654fc78
tsotnep <tsotnep@gmail.com>
2017-01-27 13:30:11 +0200

    either: moved a file, changed variable name, formatted the code or something simillar, no need for special message

---
+++ tmp/projects/ETSE_GDSP_DMA/ETSE_GDSP_DMA.sdk/simple_transfer_2/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../simple_transfer_2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../simple_transfer_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../simple_transfer_2_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../simple_transfer_2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../simple_transfer_2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../simple_transfer_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../simple_transfer_2_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../simple_transfer_2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:

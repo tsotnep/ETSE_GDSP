c2ead8c3b7f725686a0b1acce343eaf2f154a67b
tsotnep <tsotnep@gmail.com>
2017-01-27 15:28:03 +0200

    either: moved a file, changed variable name, formatted the code or something simillar, no need for special message

--- src/tcl/vivado/old/program_fpga_bit_elf.sh
+++ src/tcl/vivado/old/program_fpga_bit_elf.sh
@@ -1,2 +1,2 @@
 source /cad/x_16/SDK/2016.1/settings64.sh
-xmd -tcl /home/tsotne/ownCloud/git/ETSE_GDSP/src/tcl/vivado/old/tcl/program_fpga_bit_elf.tcl
+xmd -tcl /home/tsotne/ownCloud/git/ETSE_GDSP/src/tcl/vivado/old/program_fpga_bit_elf.tcl

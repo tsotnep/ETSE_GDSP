e35e3f2ddadc9c52f1e42f5d96f75a8839b8b1e5
tsotnep <tsotnep@gmail.com>
2017-02-13 16:13:34 +0200

    manual modif

--- src/tcl/program_fpga_BITSTREAM_ARM-APPLICATION.tcl
+++ src/tcl/program_fpga_BITSTREAM_ARM-APPLICATION.tcl
@@ -11,6 +11,7 @@ set origin /home/tsotne/ownCloud/git/ETSE_GDSP
   #before you run this script, on your PC - you should listen to uart that is connected to zedboard zynq 7000 (note that another uart is for programming it)
     #in Linux : picocom -b 115200 /dev/ttyACM0
     #or in SDK, in SDK Terminal : add new connection to serial port with port name "/dev/ttyACM0" (in windows port name will be different)
+    #if you run the script before reading previous two lines, rerun the script or type '0' in terminal
   #Reset connection with FPGA, then connect and Reset FPGA
   #Program FPGA with bitstream
   #Initialize ZYNQ processing system with default script

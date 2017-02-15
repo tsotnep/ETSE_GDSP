1ce15e60965989730070632576fe40144194b3fa
tsotnep <tsotnep@gmail.com>
2017-02-15 21:35:43 +0200

    either: moved a file, changed variable name, formatted the code or something simillar, no need for special message

--- src/tcl/other/interesting.tcl
+++ src/tcl/other/interesting.tcl
@@ -22,12 +22,10 @@ CURRENT_STEP CLASS CONSTRSET DESCRIPTION DIRECTORY STATS.ELAPSED STATS.TNS STATS
 # somehow it doest not even go in spam, anymore.
 
 set DATE [exec date]
-
 set msg {From: VIVADO_BITSTREAM@tsotnep-pc.com}
 append msg \n "To: "  [join tsotnep@gmail.com]
 append msg \n "Subject: VIVADO_JOB_START"
 append msg \n\n job start date : $DATE
-
 exec /usr/lib/sendmail -oi -t << $msg
 
 

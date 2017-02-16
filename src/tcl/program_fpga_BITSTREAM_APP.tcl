00e5391f4cee5feeaf37b3854e194a9e43983b65
tsotnep <tsotnep@gmail.com>
2017-02-16 14:48:39 +0200

    working programmer

--- src/tcl/program_fpga_BITSTREAM_APP.tcl
+++ src/tcl/program_fpga_BITSTREAM_APP.tcl
@@ -1,12 +1,23 @@
 set origin /home/tsotne/ownCloud/git/ETSE_GDSP
-# source ${origin}/src/tcl/program_fpga_BITSTREAM_ARM-APPLICATION.tcl
+# source /cad/x_16/Vivado/2016.1/settings64.sh
+# xmd -tcl /home/tsotne/ownCloud/git/ETSE_GDSP/src/tcl/program_fpga_BITSTREAM_APP.tcl
+
+#setting Hardware Design and Application names
+  #if you are programming app: FSBL
+  # set HW hw_0
+  # set NAME FSBL
+
+  #if you are programming app: ETSE_GDSP_DMA
+  # set HW hw_1
+  # set NAME ETSE_GDSP_DMA
+
+  #if you are programming app: ETSE_GDSP_DMA_ETHERNET
+  set HW hw_2
+  set NAME ETSE_GDSP_DMA_ETHERNET
 
 #How to run
-  #correct line 1
+  #correct line 1:15
   #launch XMD (either in SDK (Xilinx Tools > XMD console), or in linux terminal by sourcing xilinx's settings64.sh and then typing : xmd )
-  #source this script by:
-    # set origin /home/tsotne/ownCloud/git/ETSE_GDSP
-    # source $origin/src/tcl/program_fpga_BITSTREAM_ARM-APPLICATION.tcl
 
 #What it does and what you have to do:
   #before you run this script, on your PC - you should listen to uart that is connected to zedboard zynq 7000 (note that another uart is for programming it)
@@ -23,9 +34,9 @@ set origin /home/tsotne/ownCloud/git/ETSE_GDSP
 xdisconnect -cable
 connect arm hw
 rst -srst
-fpga -f $origin/tmp/projects/ETSE_GDSP_DMA_ETHERNET/ETSE_GDSP_DMA_ETHERNET.sdk/design_1_wrapper_hw_platform_0/design_1_wrapper.bit
-source $origin/tmp/projects/ETSE_GDSP_DMA_ETHERNET/ETSE_GDSP_DMA_ETHERNET.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
+fpga -f $origin/tmp/projects/ETSE_GDSP_DMA_ETHERNET/ETSE_GDSP_DMA_ETHERNET.sdk/$HW/design_1_wrapper.bit
+source $origin/tmp/projects/ETSE_GDSP_DMA_ETHERNET/ETSE_GDSP_DMA_ETHERNET.sdk/$HW/ps7_init.tcl
 ps7_init
 ps7_post_config
-dow $origin/tmp/projects/ETSE_GDSP_DMA_ETHERNET/ETSE_GDSP_DMA_ETHERNET.sdk/ETSE_GDSP_DMA/Debug/ETSE_GDSP_DMA.elf
+dow $origin/tmp/projects/ETSE_GDSP_DMA_ETHERNET/ETSE_GDSP_DMA_ETHERNET.sdk/$NAME/Debug/$NAME.elf
 run
